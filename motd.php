a4c2dfecc9cb52616927bebbdaa21de508414306
Bobyian <tunegagtt@gmail.com>
2017-10-08 19:09:46 +0300

    Create motd.php

---
+++ motd.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<style type="text/css">
+body {
+	font-family: Trebuchet MS;
+	text-align:center;
+}
+</style>
+</head>
+
+<body>
+<font size="48">Bulgarian Metrostroi Server</font>
+</body>
+
+</html>
